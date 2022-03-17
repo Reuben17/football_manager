1410bd22fe8282ce0df69beac796efb8042931f2
Reuben Mbalanya <75217282+Reuben17@users.noreply.github.com>
2022-03-17 14:37:29 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset ="utf-8">
+<title>Football Manager</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<link rel="stylesheet" type="text/css" href="css/index.php">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+ 
+</head>
+
+<body>
+<div class="div1">
+<h id ="heading">Football Manager Application</h>
+<div class="div2"> 
+ <p class="paragraphs"><a class="tags" href="php/authentication_module/login.php">Login</a></p>
+ <p class="paragraphs"><a class="tags" href=php/authentication_module/register.php>Register</a></p>
+              
+ </div>
+</div>
+               
+
+</body>
+</html>
